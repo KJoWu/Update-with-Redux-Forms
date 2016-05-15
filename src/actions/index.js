f299c2bb8c798b9bbcd0c1ee6bc7f85bfcdb8063
Silver Rook <kwu1@ualberta.ca>
2016-05-14 21:38:05 -0600

    updated root URL

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,10 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POST = 'FETCH_POST';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=lkajdsdfsdsfapipwietpw';
+const ROOT_URL = 'http://jsonplaceholder.typicode.com';
 
 export function fetchPosts() {
-  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/posts`);
 
   return {
     type: FETCH_POSTS,
