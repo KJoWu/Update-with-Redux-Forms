407ddb70bfa2a83d50d1bc4e9fd23539a990325f
Silver Rook <kwu1@ualberta.ca>
2016-05-14 22:42:55 -0600

    added redux-forms

--- src/components/posts_edit.js
+++ src/components/posts_edit.js
@@ -2,6 +2,22 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
 import { Link } from 'react-router';
+import { reduxForm} from 'redux-form';
+import {initialValues} from 'redux-form';
+
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a username';
+  }
+  if(!values.body) {
+    errors.body = 'Enter some content';
+  }
+
+  return errors;
+}
 
 class PostsEdit extends Component {
   static contextTypes = {
@@ -13,19 +29,30 @@ class PostsEdit extends Component {
   }
 
   render() {
-    const { post } = this.props;
-
-    if (!post) {
-      return <div>Loading...</div>;
-    }
+    const { fields: { title, body }, handleSubmit } = this.props;
 
     return (
-      <div>
-        <Link to="/">Back To Index</Link>
-        <h3>{post.title}</h3>
-        <hr></hr>
-        <p>{post.body}</p>
-      </div>
+      <form>
+        <h3>Create A New Post</h3>
+
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
+        </div>
+
+        <div className={`form-group ${body.touched && body.invalid ? 'has-danger' : ''}`}>
+          <label>Body</label>
+          <textarea className="form-control" {...body} />
+          <div className="text-help">
+            {body.touched ? body.error : ''}
+          </div>
+        </div>
+
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
     );
   }
 }
@@ -34,4 +61,13 @@ function mapStateToProps(state) {
   return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsEdit);
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title','body'],
+  validate
+}, null, { fetchPost })(PostsEdit);
+
+
+
